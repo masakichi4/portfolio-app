2a80badfe1bc2a6b0ad830e8dc3cb02c3a3b66e0
masakichi4 <mackenziejiang@berkeley.edu>
2020-01-20 11:27:18 -0800

    Updates

---
+++ precache-manifest.ccdd2b36ef1d42e1899584b7bd1ea324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe44a7e2243f86a9704ca3d0d31bc86",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "ef072fff6a19ad4cca49",
+    "url": "/portfolio-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "569b5eb029665d79e82c",
+    "url": "/portfolio-app/static/js/2.e9aec6d9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio-app/static/js/2.e9aec6d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef072fff6a19ad4cca49",
+    "url": "/portfolio-app/static/js/main.b6dcebdc.chunk.js"
+  },
+  {
+    "revision": "3986003e6b3c41b897c3",
+    "url": "/portfolio-app/static/js/runtime-main.922285d2.js"
+  }
+]);
