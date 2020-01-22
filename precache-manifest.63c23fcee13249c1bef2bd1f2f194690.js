d33ce454352268bad51169a8630ced0aa592de12
masakichi4 <mackenziejiang@berkeley.edu>
2020-01-22 00:22:31 -0800

    Updates

---
+++ precache-manifest.63c23fcee13249c1bef2bd1f2f194690.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68270a322c6d7031bb0b74a1c966bc7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0da6ac9557ac8f16306f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6eddd38ab4c8aee9ac18",
+    "url": "/static/js/2.74e5fc1d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.74e5fc1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0da6ac9557ac8f16306f",
+    "url": "/static/js/main.4c518b6f.chunk.js"
+  },
+  {
+    "revision": "5a5378621f73c4029f77",
+    "url": "/static/js/runtime-main.0b74a16b.js"
+  },
+  {
+    "revision": "a034ac03d33c0ed24a358ab0fd2b9618",
+    "url": "/static/media/about.a034ac03.jpeg"
+  },
+  {
+    "revision": "a130eb5afd4ff60ddf882565bca029ed",
+    "url": "/static/media/plants.a130eb5a.jpeg"
+  },
+  {
+    "revision": "0b2535fa5c9118b1e84896676974e2ca",
+    "url": "/static/media/tea.0b2535fa.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-app/precache-manifest.1ded15ed25dd96b7d12201bfa60cfe9c.js"
+  "/precache-manifest.63c23fcee13249c1bef2bd1f2f194690.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
